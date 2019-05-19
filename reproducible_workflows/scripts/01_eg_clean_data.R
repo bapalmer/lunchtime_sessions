38d23b699fcb2b061227e94997ba0bfd621ba172
Palmer <b.palmer@ucc.ie>
2019-05-20 00:56:02 +0100

    Open science talk PG6015

---
+++ reproducible_workflows/scripts/01_eg_clean_data.R
@@ -0,0 +1,42 @@
+#########################################################################
+# Example R-project 
+# 20th May 2019
+# 01_eg_clean_data.R
+#########################################################################
+
+# Load the required packages
+
+library(tidyverse)
+
+# Load and clean the data -------------------------------------------------
+
+who_tb_data <- read_csv("data/raw_data/who_tb_data.csv",
+                        skip = 2) %>%
+  
+  gather(new_ep_f014:new_sp_m65, # columns to be collapsed
+         key = "key", 
+         value = "cases", 
+         na.rm = TRUE) %>%
+  
+  # Two separate steps required to process the "key" column
+  separate(key, 
+           c("new", "type", "sexage"), 
+           sep = "_") %>%
+  
+  separate(sexage, 
+           c("sex", "age"), 
+           sep = 1) %>%
+  
+  select(-iso2, -iso3, -new) %>% # Removes the redundant columns 
+  
+  spread(sex, cases) %>% # Places males and females in separate columns
+  
+  mutate(total = m + f)
+
+# Save a copy of the clean data -------------------------------------------
+
+write_csv(who_tb_data,
+          paste("data/",
+                Sys.Date(),
+                "_clean_who_tb_data.csv",
+                sep = ""))
